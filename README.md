2a11e36f38b840b733f2cb0810744451735fdaa6
李樟取 <lizhangqu@weidian.com>
2017-10-31 21:47:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+### Android Gradle Plugin代码碎片
+
+具体功能切换分支查看
---
+++ buildSrc/src/main/groovy/io/github/lizhangqu/plugin/SamplePlugin.groovy
@@ -0,0 +1,10 @@
+package io.github.lizhangqu.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SamplePlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+    }
+}
